70e13d3e4655694f0000ec0d3ea2d2011d952299
Zabir Raihan <zabirraihan6@gmal.com>
2023-04-06 05:16:11 +0800

    Initial Commit

---
+++ SwiftNews/SwiftNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNewsApp.swift
+//  SwiftNews
+//
+//  Created by Zabir Raihan on 06/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
